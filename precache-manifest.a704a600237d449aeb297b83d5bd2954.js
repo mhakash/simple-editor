7466da6133cd5a6c6591334ec46fd5581c4faf22
mhakash <mhakash211998@gmail.com>
2020-08-14 15:14:01 +0600

    Updates

---
+++ precache-manifest.a704a600237d449aeb297b83d5bd2954.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e98195bbe702a7fc1d5958b8cb876a4d",
+    "url": "/mhakash/simple-editor/index.html"
+  },
+  {
+    "revision": "62d7d0b780650a70d947",
+    "url": "/mhakash/simple-editor/static/css/2.fe1c1d21.chunk.css"
+  },
+  {
+    "revision": "3211ad4ee741204cf66b",
+    "url": "/mhakash/simple-editor/static/css/main.de964391.chunk.css"
+  },
+  {
+    "revision": "62d7d0b780650a70d947",
+    "url": "/mhakash/simple-editor/static/js/2.7c284b77.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mhakash/simple-editor/static/js/2.7c284b77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3211ad4ee741204cf66b",
+    "url": "/mhakash/simple-editor/static/js/main.929f2391.chunk.js"
+  },
+  {
+    "revision": "c58198a48744e1a14939",
+    "url": "/mhakash/simple-editor/static/js/runtime-main.0d6ddc70.js"
+  }
+]);
